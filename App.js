0f65a828298ea394f2d66820d33831f40d46afba
Besal Raj <besalraj@Besals-MacBook-Pro.local>
2024-04-22 13:08:38 +0530

    Ep 2. Igniting our App

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement(
+    "div",
+    {
+      id: "parent",
+    },
+    [
+        React.createElement("div", { id: "child1"}, [
+            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h2", {}, "I'm an h2 tag")
+        ]),
+        React.createElement("div", {id: "child2"}, [
+            React.createElement("h1", {}, "I'm an h1 tag"),
+            React.createElement("h2", {}, "I'm an h2 tag")
+        ]),
+    ]
+  );
+  console.log(parent);
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  //passing a react element inside the root
+  //async defer
+  root.render(parent);
---
+++ READEME.md
@@ -0,0 +1,18 @@
+Parcel:
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- Uses a File Watching Algorithm written in C++
+- Caching -> faster builds
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling -> support older browsers
+- Diagnostic
+- Error handling
+- HTTPS
+- Tree Shaking -> remove unused code
+- Different dev and prod bundles -> npx parcel build index.html
